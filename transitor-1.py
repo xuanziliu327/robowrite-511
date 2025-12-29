cc0e90e396406dfbcdd356d498596dbbc679a9d3
wuwuka11 <wangly22025@shanghaitech.edu.cn>
2025-12-29 14:57:55 +0800

    Add files via upload

--- transitor-1.py
+++ transitor-1.py
@@ -6,7 +6,7 @@ a=0
 p=input().split()
 n=[]
 for i in p:
-    t=int(i)
+    t=float(i)
     n.append(t)
 c=np.zeros((len(n)//2,3))
 while a<len(n)/2:
@@ -17,10 +17,17 @@ while a<len(n)/2:
 
 
     a+=1
-print(c)
+
 #90 41 490 41 490 241 90 241
 #[[0.25 0.1  0.1 ]
  #[0.25 0.6  0.1 ]
  #[0.5  0.6  0.1 ]
  #[0.5  0.1  0.1 ]]
 #边框数据测试
+for i in range(49):
+    print(f"q{i}=np.array([{c[i][0]},{c[i][1]},{c[i][2]}])")
+for i in range(52):
+    print(f"a{i+1}=np.array([{c[i+49][0]},{c[i+49][1]},{c[i+49][2]}])")
+for i in range(105):
+    print(f"b{i+1}=np.array([{c[i+101][0]},{c[i+101][1]},{c[i+101][2]}])")
+
