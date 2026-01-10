0019986ef495784ab9c6a7197c39a0c0768675a4
Liu Zixuan <liuzixuan3823@126.com>
2026-01-11 01:16:35 +0800

    Add files via upload

--- transitor-1.py
+++ transitor-1.py
@@ -25,9 +25,21 @@ while a<len(n)/2:
  #[0.5  0.1  0.1 ]]
 #边框数据测试
 for i in range(49):
-    print(f"q{i}=np.array([{c[i][0]},{c[i][1]},{c[i][2]}])")
+    if i==3 or i==6 or i==14 or i==19 or i==22 or i==25 or i==28 or i==36 or i==40 or i==43 or i==48:
+        c[i][2] +=0.02
+        print(f"q{i}=np.array([{c[i][0]},{c[i][1]},{c[i][2]}])")
+    else:
+        print(f"q{i}=np.array([{c[i][0]},{c[i][1]},{c[i][2]}])")
 for i in range(52):
-    print(f"a{i+1}=np.array([{c[i+49][0]},{c[i+49][1]},{c[i+49][2]}])")
+    if  i==3 or i==6 or i==10 or i==14 or i==17 or i==21 or i==27 or i==30 or i==33 or i==37 or i==40 or i==43 or i==46 or i==49:
+        c[i+49][2] +=0.02
+        print(f"a{i+1}=np.array([{c[i+49][0]},{c[i+49][1]},{c[i+49][2]}])")
+    else:
+        print(f"a{i+1}=np.array([{c[i+49][0]},{c[i+49][1]},{c[i+49][2]}])")
 for i in range(105):
-    print(f"b{i+1}=np.array([{c[i+101][0]},{c[i+101][1]},{c[i+101][2]}])")
+    if i ==8 or i==18 or i==26 or i==38 or i==50 or i==64 or i==78 or i==82 or i==94:
+        c[i+101][2] +=0.02
+        print(f"b{i+1}=np.array([{c[i+101][0]},{c[i+101][1]},{c[i+101][2]}])")
+    else:
+        print(f"b{i+1}=np.array([{c[i+101][0]},{c[i+101][1]},{c[i+101][2]}])")
 
